7a973a527cb51aed24d2a0fa2dd2a125d4d47aac
Sebastian Ekman <sebastian.ekman@gmail.com>
2018-01-11 15:20:35 +0100

    Removed write to data file

--- resources/lib/youtube_plugin/youtube/helper/video_info.py
+++ resources/lib/youtube_plugin/youtube/helper/video_info.py
@@ -589,10 +589,6 @@ class VideoInfo(object):
         data = result.text
         params = dict(urlparse.parse_qsl(data))
 
-        with open('/storage/somefile.txt', 'w') as the_file:
-            the_file.write(data)
-
-
         meta_info = {'video': {},
                      'channel': {},
                      'images': {},
